99e723e34abc31bebf3e47266b4696a8be8e7e45
ashrit <ashrit09@gmail.com>
2017-03-01 12:53:20 -0500

    Bug fixes

--- Image-Convolution.cu
+++ Image-Convolution.cu
@@ -10,10 +10,19 @@
 #define TILE_WIDTH 16
 #define BLOCKSIZE 256
 
-__global__ void 1Dconv(int *outputArray, int *inputArray, int *kernel, int length)
+__global__ void 1Dconv(int *outputArray, int *inputArray, int arg, int length)
 {
+	int x, i;
 	__shared__ int shared_array[BLOCKSIZE+2];
-	int x = threadIdx.x + blockIdx.x * blockDim.x;
+	int kernel1D[3][3] = {1,1,1,0,0,0,1,0,1};
+	x = threadIdx.x + blockIdx.x * blockDim.x;
+	int kernel[3];
+
+	for(i = 0; i < 3; i++)
+	{
+		kernel[i] = kernel1D[arg][i];
+	}
+
 	if((threadIdx.x == 0) && (blockIdx.x == 0))
 	{
 		shared_array[0] = 0;
@@ -32,15 +41,29 @@ __global__ void 1Dconv(int *outputArray, int *inputArray, int *kernel, int lengt
 	}
 	shared_array[x+1] = inputArray[x];
 	__syncthreads();
-	outputArray[x] = (shared_array[x]*kernel[0]) + (shared_array[x+1]*kernel[1]) + (shared_array[x+2]*kernel[2]);
+
+	if(x < length)
+		outputArray[x] = (shared_array[x]*kernel[0]) + (shared_array[x+1]*kernel[1]) + (shared_array[x+2]*kernel[2]);
 }
 
-__global__ void 2Dconv(int *outputImage, int *inputImage, int *kernel, int width, int height)
+__global__ void 2Dconv(int *outputImage, int *inputImage, int arg, int width, int height)
 {
-	int i, j, pixel;
+	int i, j, k, pixel;
 	__shared__ int shared_image[TILE_WIDTH+2][TILE_WIDTH+2];
+	int kernel2D[3][9] = {-9,-9,-9,-9,72,-9,-9,-9,-9,0,-9,0,-9,45,-9,0,-9,0,1,1,1,1,1,1,1,1,1};
 	int x = threadIdx.x + blockIdx.x * blockDim.x;
 	int y = threadIdx.y + blockIdx.y * blockDim.y;
+	int kernel[3][3];
+
+	int k = 0;
+	for(i = 0; i < 3; i++)
+	{
+		for(j = 0; j < 3; j++)
+		{
+			kernel[i][j] = kernel2D[arg][k];
+			k++;
+		}
+	}
 
 	if(blockIdx.x == 0)
 		shared_image[0][y+1] = 0;
@@ -76,16 +99,11 @@ __global__ void 2Dconv(int *outputImage, int *inputImage, int *kernel, int width
 
 void do_1D_conv(char **argv, int *kernel1D)
 {
-  int arrayLength, i;
+  int arrayLength, arg, i;
 	int *hostInput, *hostOutput;
 	int *deviceInput, *deviceOutput;
-	int kernel[3]
 
-	for(i = 0; i < 3; i++)
-	{
-		kernel[i] = kernel1D[atoi(argv[2])][i];
-	}
-
+	arg = atoi(argv[2]);
 	read_array(argv[3], &hostInput, &arrayLength);
 	hostOutput = (int *)calloc(arrayLength, sizeof(int));
 	cudaMalloc((void **)&deviceInput, arrayLength * sizeof(int));
@@ -93,7 +111,7 @@ void do_1D_conv(char **argv, int *kernel1D)
 	cudaMemcpy(deviceInput, hostInput, arrayLength * sizeof(int), cudaMemcpyHostToDevice);
 	dim3 dimGrid(ceil(arrayLength / BLOCKSIZE, 1, 1);
 	dim3 dimBlock((BLOCKSIZE), 1, 1);
-	1Dconv <<< dimGrid, dimBlock >>> (deviceOutput, deviceInput, kernel, arrayLength);
+	1Dconv <<< dimGrid, dimBlock >>> (deviceOutput, deviceInput, arg, arrayLength);
 	cudaMemcpy(hostOutput, deviceOutput, arrayLength * sizeof(int), cudaMemcpyDeviceToHost);
 	write_image_template<int>(argv[4], hostOutput, arrayLength);
 	cudaFree(deviceInput);
@@ -101,23 +119,13 @@ void do_1D_conv(char **argv, int *kernel1D)
 	free(hostOutput);
 }
 
-void do_2D_conv(char **argv, int *kernel2D)
+void do_2D_conv(char **argv)
 {
-	int imageWidth, imageHeight, img_size, i, j, k;
+	int imageWidth, imageHeight, img_size, arg, i, j, k;
 	int *hostInput, *hostOutput;
 	int *deviceInput, *deviceOutput;
-	int kernel[3][3];
 
-  k = 0;
-	for(i = 0; i < 3; i++)
-	{
-		for(j = 0; j < 3; j++)
-		{
-			kernel[i][j] = kernel2D[atoi(argv[2])][k];
-			k++;
-		}
-	}
-
+	arg = atoi(argv[2]);
 	read_image_template<int>(argv[3], &hostInput, &imageWidth, &imageHeight);
 	img_size = imageWidth * imageHeight;
 	hostOutput = (int *)calloc(img_size, sizeof(int));
@@ -126,7 +134,7 @@ void do_2D_conv(char **argv, int *kernel2D)
 	cudaMemcpy(deviceInput, hostInput, img_size * sizeof(int), cudaMemcpyHostToDevice);
 	dim3 dimGrid(ceil(imageWidth / TILE_WIDTH), ceil(imageHeight / TILE_WIDTH));
 	dim3 dimBlock(TILE_WIDTH, TILE_WIDTH, 1);
-	2Dconv <<< dimGrid, dimBlock >>> (deviceOutput, deviceInput, kernel, imageWidth, imageHeight);
+	2Dconv <<< dimGrid, dimBlock >>> (deviceOutput, deviceInput, arg, imageWidth, imageHeight);
 	cudaMemcpy(hostOutput, deviceOutput, img_size * sizeof(int), cudaMemcpyDeviceToHost);
 	write_image_template<int>(argv[4], hostOutput, imageWidth, imageHeight);
 	cudaFree(deviceInput);
@@ -144,18 +152,13 @@ int main (int argc, char **argv)
 		exit(1);
 	}
 
-  int kernel1D[3][3];
-	int kernel2D[9][9];
-
 	if(atoi(argv[1]) == 0)
 	{
-		kernel1D[3][3] = {1,1,1,0,0,0,1,0,1};
-		do_1D_conv(argv, kernel1D);
+		do_1D_conv(argv);
 	}
 	else
 	{
-		kernel2D[3][9] = {-9,-9,-9,-9,72,-9,-9,-9,-9,0,-9,0,-9,45,-9,0,-9,0,1,1,1,1,1,1,1,1,1};
-		do_2D_conv(argv, kernel2D);
+		do_2D_conv(argv);
 	}
 
 	return 0;
